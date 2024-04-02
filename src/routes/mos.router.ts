74c4caad6a2fad0364119b85dd6105d27162bb2e
abharun <adambinharun1010@gmail.com>
2024-04-02 20:13:29 +0200

    metrics for payment per store and retailer

--- src/routes/mos.router.ts
+++ src/routes/mos.router.ts
@@ -1,12 +1,9 @@
 import express from "express";
-import { getMosMetrics, updateMosMetricsHist, updateMosMetricsGauge, updateMosMetricsSummary, updateMosMetricsPayment } from "../controllers";
+import { getMosMetrics, updateMosMetrics } from "../controllers";
 
 const router = express.Router();
 
 router.get('/', getMosMetrics);
-router.post('/gauge', updateMosMetricsGauge);
-router.post('/hist', updateMosMetricsHist);
-router.post('/summary', updateMosMetricsSummary);
-router.post('/payment', updateMosMetricsPayment);
+router.post('/', updateMosMetrics);
 
 export default router;
